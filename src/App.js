e20f09cf73c96ca073bf394adb0d080d1d77dcb7
Pankaj Kumar <pankajkumardas727@gmail.com>
2021-08-01 22:29:20 +0530

    submissionDemo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SuperFluid from './pages/SuperFluid';
 
 function App() {
 
@@ -12,8 +10,6 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/superfluid" component={SuperFluid} />
-          <Route path="/signinportis" component={SignIn} />
         </Switch>
       </Router>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import SuperfluidSDK from '@superfluid-finance/js-sdk'
 import Web3 from 'web3'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 // let bobAdd = "0x563F9bcF8cbc7A102608e41e36B3C0d2547ae9cE"
 let aliceAddress = "0x7D98112f0Cf86fADcF09EdDbbE1BEEe4381D394f"
@@ -14,8 +15,8 @@ function Home() {
     
     portis.onLogin((walletAddress, email, reputation) => {
         console.log(walletAddress, email, reputation);
-        document.getElementById("walletAdd").innerHTML = "Wallet Address " + walletAddress; 
-        document.getElementById("email").innerHTML = "Email " + email;
+        document.getElementById("walletAdd").innerHTML = walletAddress; 
+        document.getElementById("email").innerHTML = email;
     });
     
     portis.isLoggedIn().then(({ error, result }) => {
@@ -40,22 +41,24 @@ function Home() {
 
                 // SF initiaziled
                 const sf = new SuperfluidSDK.Framework({
-                    web3: new Web3(portis.provider)
+                    // web3: new Web3(portis.provider)
+                    web3: new Web3(window.ethereum)
                 })
                 await sf.initialize()
 
                 // create user
-                // const walletAddress = await window.ethereum.request({
-                //     method: 'eth_requestAccounts',
-                //     params: [
-                //     {
-                //         eth_accounts: {}
-                //     }
-                //     ]
-                // });
+                const walletAddress = await window.ethereum.request({
+                    method: 'eth_requestAccounts',
+                    params: [
+                    {
+                        eth_accounts: {}
+                    }
+                    ]
+                });
 
                 const userBob = sf.user({
-                    address: "0xbd225264c591720369b98de561c4cbe01ddd2e14",
+                    // address: "0xbd225264c591720369b98de561c4cbe01ddd2e14",
+                    address: walletAddress[0],
                     token: "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f",
                     // 0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f fDAIx token
                     //0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00 original
@@ -82,22 +85,41 @@ function Home() {
         )
     }
 
+    // GrapgQL
+    const QUERY_URL = "https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-mumbai"
+
+    const query = `{
+        account(id: "0x7D98112f0Cf86fADcF09EdDbbE1BEEe4381D394f") {
+            flowOwned {
+                flowRate
+                sum
+                lastUpdated
+                token {
+                    id
+                    symbol
+                }
+            }
+        }
+    }` 
+
+    const result = axios.post(QUERY_URL, {query})
+    console.log("result ", result);
+
     return (
         <div className="App">
-            <h1>
-            <Link to="/" className="portis-button">Home</Link>
-            </h1>
-            <h1>
             <button onClick={() => start()} className="portis-button">Start</button>
             <button className="portis-button">Stop</button>
-            </h1>
             <Link to="/superfluid" className="portis-button">Go to SuperFluid</Link>
             <button onClick={() => portis.provider.enable()} className="portis-button">Login to Wallet</button>
             <button onClick={() => portis.logout()} className="portis-button">Log Out</button>
-            <h1 id="walletAdd">Address </h1>
-            <h1 id="email">Email </h1>
-            <h1 id="geoDataTime">Time 0</h1>
-            <h1 id="geoDataSpeed">Speed 0</h1>
+            <div className="main">
+            <h3 id="">Address </h3>
+            <p id="walletAdd"> </p>
+            <h3 id="">Email </h3>
+            <p id="email"> </p>
+            <h3 id="geoDataTime">Time 0</h3>
+            <h3 id="geoDataSpeed">Speed 0</h3>
+            </div>
         </div>
     )
 }
