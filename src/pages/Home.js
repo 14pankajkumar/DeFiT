2cf0e56160f999427ed8cf6bfce168cb4b7aba48
Pankaj Kumar <pankajkumardas727@gmail.com>
2021-08-07 18:11:09 +0530

    demotest4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import Portis from '@portis/web3'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SuperfluidSDK from '@superfluid-finance/js-sdk'
 import Web3 from 'web3'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-// let bobAdd = "0x563F9bcF8cbc7A102608e41e36B3C0d2547ae9cE"
-let aliceAddress = "0x7D98112f0Cf86fADcF09EdDbbE1BEEe4381D394f"
+const bobAdd = "0xbd225264c591720369b98de561c4cbe01ddd2e14"
+const aliceAddress = "0x7D98112f0Cf86fADcF09EdDbbE1BEEe4381D394f"
+
 function Home() {
+    const [count, setCount] = useState()
+    const myLocalPOANode = {
+        nodeUrl: "https://matic-mumbai.chainstacklabs.com",
+        chainId: 80001,
+      };
     // Posrtis login
-    const portis = new Portis('835530b2-c62e-4b03-86f0-5795ae704490', 'maticMumbai', {
+    const portis = new Portis('835530b2-c62e-4b03-86f0-5795ae704490', myLocalPOANode, {
         scope: ["email", "reputation"]
     })
     
@@ -39,44 +45,44 @@ function Home() {
                 document.getElementById("geoDataTime").innerHTML = "Time " + data.timestamp;
                 document.getElementById("geoDataSpeed").innerHTML = "Speed " + data.coords.speed;
 
-                // SF initiaziled
                 const sf = new SuperfluidSDK.Framework({
-                    // web3: new Web3(portis.provider)
-                    web3: new Web3(window.ethereum)
-                })
+                    web3: new Web3(portis.provider),
+                });
                 await sf.initialize()
 
-                // create user
-                const walletAddress = await window.ethereum.request({
-                    method: 'eth_requestAccounts',
-                    params: [
-                    {
-                        eth_accounts: {}
-                    }
-                    ]
-                });
-
+                // const walletAddress = await window.ethereum.request({
+                //         method: 'eth_requestAccounts',
+                //         params: [
+                //         {
+                //             eth_accounts: {}
+                //         }
+                //         ]
+                //     });
+                        
+                // const bobAddress = walletAddress[0] // address of the sender's wallet
                 const userBob = sf.user({
-                    // address: "0xbd225264c591720369b98de561c4cbe01ddd2e14",
-                    address: walletAddress[0],
-                    token: "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f",
-                    // 0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f fDAIx token
-                    //0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00 original
+                address: bobAdd, 
+                token: "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f" // address of the Super Token
                 });
 
-                // setInterval(() => {
+                // await userBob.flow({
+                //     recipient: aliceAddress,
+                //     flowRate: "385802469135802"
+                //     });
+
                 if (data.coords.speed > 1) {
                     await userBob.flow({
-                        recipient: aliceAddress, // my recipientAcc
-                        flowRate: '385802469135802'
-                    });
+                        recipient: aliceAddress,
+                        flowRate: "385802469135802"
+                        });
                 } else {
                     await userBob.flow({
-                        recipient: aliceAddress, // my recipientAcc
-                        flowRate: '0'
-                    });
+                        recipient: aliceAddress,
+                        flowRate: "0"
+                        });
                 }
-                // }, 1000)
+                
+                    
             },
             error => console.log(error),
             {
@@ -86,30 +92,64 @@ function Home() {
     }
 
     // GrapgQL
-    const QUERY_URL = "https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-mumbai"
-
-    const query = `{
-        account(id: "0x7D98112f0Cf86fADcF09EdDbbE1BEEe4381D394f") {
-            flowOwned {
-                flowRate
-                sum
-                lastUpdated
-                token {
-                    id
-                    symbol
+    const main = async () => {
+        try {
+            const result = await axios.post(
+                "https://api.thegraph.com/subgraphs/name/superfluid-finance/superfluid-mumbai",
+                {
+                    query : `{
+                        account(
+                            id: "0xbd225264c591720369b98de561c4cbe01ddd2e14"
+                        ) {
+                            flowsOwned {
+                                flowRate
+                                sum
+                                lastUpdate
+                                token {
+                                    id
+                                    symbol
+                                }
+                            }
+                            flowsReceived {
+                                id
+                            }
+                        }
+                    }` 
                 }
-            }
+            )
+            console.log(result.data);
+        } catch(error) {
+            console.log(error);
         }
-    }` 
+    }
 
-    const result = axios.post(QUERY_URL, {query})
-    console.log("result ", result);
+    main()
+    
+    
+    // calculating totals
+    const now = new Date()
+    const THREE_HOURS_AGO = new Date(now - 3 * 3600)
+    const lastUpdate = THREE_HOURS_AGO
+    const sum = 0
+    const flowRate = 0.00027778 // 1 per hour
+
+    const totalFlowed = sum + flowRate * (now - lastUpdate)
+
+    console.log(`Total flowed: ${totalFlowed} tokens`);
+
+    // Streams are live
+    const interestEarnedIn2Seconds = flowRate * 2
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCount(count + interestEarnedIn2Seconds);
+        }, 2000);
+        return () => clearTimeout(timer);
+    })
 
     return (
         <div className="App">
-            <button onClick={() => start()} className="portis-button">Start</button>
-            <button className="portis-button">Stop</button>
-            <Link to="/superfluid" className="portis-button">Go to SuperFluid</Link>
+            <button onClick={() => start()} className="portis-button">Start Running</button>
+            <button className="portis-button" onClick={() => window.location.reload()} >Stop Running</button>
             <button onClick={() => portis.provider.enable()} className="portis-button">Login to Wallet</button>
             <button onClick={() => portis.logout()} className="portis-button">Log Out</button>
             <div className="main">
